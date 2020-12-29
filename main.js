e28814bca9ff9895a48855e8397cf1bd374c93b7
Auer111 <44841139+Auer111@users.noreply.github.com>
2020-12-29 13:51:28 -0500

    Under Construction

--- main.js
+++ main.js
@@ -1,38 +1,63 @@
 
-document.addEventListener('readystatechange', event => {
-    if (event.target.readyState === 'complete') 
-    { 
-        // $(".slider").append(Card("head","title","text"));
-        // $(".slider").append(Card("head","title","text"));
-        // $(".slider").append(Card("head","title","text"));
+var body = document.body;
+var slider = document.querySelector(".slider");
+var rope = document.querySelector(".rope");
+var claw = document.querySelector(".claw");
+
+function showCoords(x,y) {
+    var x = x - 40;
+    var y = y - 40;
+    slider.style.left = x +"px";
+    rope.style.left = x +"px";
+    rope.style.height = (y -40) +"px";
+    claw.style.left = x +"px";
+    claw.style.top = y +"px";
+}
+function clicked(event){
+    showCoords(event.clientX,event.clientY);
+  }
+
+body.addEventListener('touchmove', function(e) {
+    showCoords(e.touches[0].clientX,e.touches[0].clientY);
+}, false);
 
 
-        $('.slick').slick({
-            centerMode: true,
-            centerPadding: '60px',
-            slidesToShow: 3,
-            responsive: [
-                {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-                },
-                {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-                }
-            ]
-            });
-    }});
+  
+
+// document.addEventListener('readystatechange', event => {
+//     if (event.target.readyState === 'complete') 
+//     { 
+//         // $(".slider").append(Card("head","title","text"));
+//         // $(".slider").append(Card("head","title","text"));
+//         // $(".slider").append(Card("head","title","text"));
+
+
+//         $('.slick').slick({
+//             centerMode: true,
+//             centerPadding: '60px',
+//             slidesToShow: 3,
+//             responsive: [
+//                 {
+//                 breakpoint: 768,
+//                 settings: {
+//                     arrows: false,
+//                     centerMode: true,
+//                     centerPadding: '40px',
+//                     slidesToShow: 3
+//                 }
+//                 },
+//                 {
+//                 breakpoint: 480,
+//                 settings: {
+//                     arrows: false,
+//                     centerMode: true,
+//                     centerPadding: '40px',
+//                     slidesToShow: 1
+//                 }
+//                 }
+//             ]
+//             });
+//     }});
 
 
 
